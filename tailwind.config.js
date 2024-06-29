50d67e48b267f93f22babfacfd818350bc5b5489
codespel <lukmanshodi44@gmail.com>
2024-06-29 19:41:08 +0700

    menambah kan gambar pada index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0f766e",
+        ungu: "#4c1d95",
+        dark: "#020617",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
